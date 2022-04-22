3f75c7b5a79a07f038798f4e0731b5fd741312d2
amillerr <artemmillerr@gmail.com>
2022-04-23 00:46:28 +0300

    feat: add Theme component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import UserInfo from "../UserInfo/UserInfo";
+import HeaderTheme from "../HeaderTheme/HeaderTheme";
 
 import "./styles.css";
+
 
 const Header = () => {
     return (
         <div className="header">
             <UserInfo />
+            <HeaderTheme/>
         </div>
     );
 };
