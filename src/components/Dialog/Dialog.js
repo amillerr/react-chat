9cef6f3ef522c3b2655efe2991ba4c4ddf8c85be
amillerr <artemmillerr@gmail.com>
2022-04-23 00:31:12 +0300

    feat: add functions addMessage/removeMessage

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -1,23 +1,67 @@
-import React from "react";
+import React, { useReducer, useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+
 import DialogItem from "../DialogItem/DialogItem";
+
 import { normalizeDialog } from "../../helpers/dialogHelper";
+import reducer from "../../helpers/reducer";
 import data from "../../config/data";
 
 import "./styles.css";
 
+const Dialog = ({ newMessage }) => {
+  const dialogRef = useRef();
+  const [state, dispatch] = useReducer(reducer, {
+    messages: data,
+  });
 
-const Dialog = () => {
-  const normalizedDialog = normalizeDialog(data);
+  useEffect(() => {
+    if (newMessage) {
+      dispatch({
+        type: "add-message",
+        payload: newMessage,
+      });
+
+      setTimeout(() => {
+        dispatch({
+          type: "update-status",
+          payload: {
+            id: newMessage.id,
+            status: "readed",
+          },
+        });
+      }, 1000);
+    }
+  }, [newMessage]);
+
+  useEffect(() => {
+    dialogRef.current.scrollTop = dialogRef.current.scrollHeight;
+  }, [state.messages.length]);
+
+  const onRemove = (id) => {
+    dispatch({
+      type: "remove-message",
+      payload: id,
+    });
+  };
+
+  const normalizedDialog = normalizeDialog(state.messages);
 
   return (
     <div className="dialog">
-      <div className="overflow">
+      <div className="overflow" ref={dialogRef}>
         {normalizedDialog.map((item) => (
-          <DialogItem {...item} key={item.id} />
+          <DialogItem {...item} key={item.id} onRemove={onRemove} />
         ))}
       </div>
     </div>
   );
+};
+
+Dialog.propTypes = {
+  newMessage: PropTypes.shape({
+    id: PropTypes.number,
+  }),
 };
 
 export default Dialog;
